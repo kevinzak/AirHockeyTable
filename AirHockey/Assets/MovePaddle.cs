9008791b022cf02dd8fe77c739aeca454f3f347d
Henry Steinberg <henry@bluecadet.com>
2018-11-06 16:04:45 -0500

    getting a paddle move

---
+++ AirHockey/Assets/MovePaddle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePaddle : MonoBehaviour
+{
+
+  private Vector3 pos;
+
+  // Use this for initialization
+  void Start()
+  {
+    Input.multiTouchEnabled = true;
+    pos = new Vector3(this.transform.position.x, this.transform.position.y, 0.5f);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetMouseButtonDown(0))
+    {
+      this.transform.position = Camera.main.WorldToScreenPoint(Input.mousePosition);
+      Debug.Log(Input.mousePosition);
+    }
+  }
+}
---
+++ AirHockey/Assets/SpawnPaddle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPaddle : MonoBehaviour
+{
+
+  public GameObject clone;
+
+  // Use this for initialization
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+    {
+      Vector2 fingerPos = Input.GetTouch(0).position;
+      // fingerPos.z = 5;
+      Debug.Log(fingerPos);
+      // Vector3 objPos = Camera.main.ScreenToWorldPoint(fingerPos);
+      // clone = Instantiate(trail, objPos, Quaternion.identity) as GameObject;
+
+    }
+  }
+}
